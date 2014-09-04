15a815c82f84d9e6dcae730bdd7ed02e8efd71a5
anengineerinchina <md.masterchief@gmail.com>
2014-09-04 15:16:21 -0400

    added jl777 function

--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -142,7 +142,7 @@ extern void EnsureWalletIsUnlocked();
 extern uint256 ParseHashV(const json_spirit::Value& v, std::string strName);
 extern uint256 ParseHashO(const json_spirit::Object& o, std::string strKey);
 extern std::vector<unsigned char> ParseHexV(const json_spirit::Value& v, std::string strName);
-extern std::vector<unsigned char> ParseHexO(const json_spirit::Object& o, std::string strKey); 
+extern std::vector<unsigned char> ParseHexO(const json_spirit::Object& o, std::string strKey);
 
 extern json_spirit::Value getconnectioncount(const json_spirit::Array& params, bool fHelp); // in rpcnet.cpp
 extern json_spirit::Value getpeerinfo(const json_spirit::Array& params, bool fHelp);
@@ -208,6 +208,7 @@ extern json_spirit::Value decodescript(const json_spirit::Array& params, bool fH
 extern json_spirit::Value signrawtransaction(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value sendrawtransaction(const json_spirit::Array& params, bool fHelp);
 //bitcoindark: teleporter RPC command
+extern json_spirit::Value jl777(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value jl777json(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value jl777_JSON(const json_spirit::Array& params, bool fHelp);
 
