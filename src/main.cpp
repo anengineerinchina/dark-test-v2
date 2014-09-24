3bc3b30251555f18c7b4843d90525bf4989fee3c
Matthew <md.masterchief@gmail.com>
2014-09-24 16:02:24 -0400

    removed libjl777_narrowcast()

--- src/main.cpp
+++ src/main.cpp
@@ -3977,10 +3977,6 @@ int32_t narrowcast(char *destip,unsigned char *msg,int32_t len) //Send a PubAddr
 	return(-2);
 }
 
-extern "C" int32_t libjl777_narrowcast(char *destip,unsigned char *msg,int32_t len)
-{
-	return (narrowcast(destip, msg, len));
-}
 
 
 void init_jl777(char *myip)
