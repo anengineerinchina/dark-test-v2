77ee5f6e053751d6f76733c02d613b4d27e48c54
neoranga <neoranga55@gmail.com>
2014-10-22 18:39:07 +0200

    Fix to test framework setup scripts

--- libjl777/tests/setup.sh
+++ libjl777/tests/setup.sh
@@ -5,21 +5,14 @@ wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.
 tar -xzvf chruby-0.3.8.tar.gz
 cd chruby-0.3.8/
 sudo make install
+cd ..
 rm -R chruby-0.3.8 chruby-0.3.8.tar.gz
 
 
 echo "Installing RVM (Ruby version manager)"
 
 curl -L https://get.rvm.io | bash -s stable
-rvm requirements
-rvm install ruby
-rvm use ruby --default
-[ -f $HOME/.bashrc ] && echo "[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm">>$HOME/.bashrc && echo "Warning: You need to open a new terminal for these changes to take effect"
-[ -f $HOME/.zshrc ] && echo "[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm">>$HOME/.zshrc && echo "Warning: You need to open a new terminal for these changes to take effect"
-rvm rubygems current
-
-echo "Installing Cucumber (behavioural testing framework) and other libraries required for testing"
-bundle install # to automatically install missing gems
-gem update
 
 sudo apt-get install expect-dev # Required to unbuffer the output of BitcoinDarkd
+
+./internal_setup.sh
