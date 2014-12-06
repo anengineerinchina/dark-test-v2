da92267284b742db09113e9ba356f9450447561b
bitcoindarkdev <bitcoindarkdev@gmail.com>
2014-12-06 01:14:40 -0500

    fixed deps

--- libjl777/mxe/src/curl.mk
+++ libjl777/mxe/src/curl.mk
@@ -19,12 +19,12 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --with-gnutls \
+        --without-gnutls \
         --without-ssl \
-        --with-libidn \
+        --without-libidn \
         --enable-sspi \
         --enable-ipv6 \
-        --with-libssh2
+        --without-libssh2
     $(MAKE) -C '$(1)' -j '$(JOBS)' install $(MXE_DISABLE_DOCS)
     ln -sf '$(PREFIX)/$(TARGET)/bin/curl-config' '$(PREFIX)/bin/$(TARGET)-curl-config'
 
